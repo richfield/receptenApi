07618638d68484b6c439dc0ffc81a840a96379b4
Richard ten Velde <richard.ten.velde@ivention.nl>
2024-12-16 10:10:44 +0100

    run port

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import recipeRoutes from "./routes/recipeRoutes";
 import scrapeRoutes from "./routes/scrapeRoutes";
-import { connectToDatabase, getDB } from './utils/dbConnection';
+import { connectToDatabase } from './utils/dbConnection';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 connectToDatabase();
 
 app.use(express.json());
