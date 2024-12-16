af9882b6fd2c3de51b1c19212351189fb0d25e33
Richard ten Velde <richard.ten.velde@ivention.nl>
2024-12-16 10:13:41 +0100

    log env

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from './utils/dbConnection';
 
 const app = express();
 const port = process.env.PORT || 3000;
+console.log({env: process.env})
 connectToDatabase();
 
 app.use(express.json());
