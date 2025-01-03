2d8e50460de808a7bf35853ff07366247097ecb1
Richard ten Velde <richard.ten.velde@ivention.nl>
2025-01-03 17:16:39 +0100

    env

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,25 @@ import recipeRoutes from './routes/recipeRoutes';
 import scrapeRoutes from './routes/scrapeRoutes';
 import admin from 'firebase-admin';
 import { Response, NextFunction } from 'express';
-import serviceAccount from './serviceAccountKey.json';
 import { AuthenticatedRequest } from './Types';
 import profileRoutes from './routes/profileRoutes';
 
 
 const app = express();
 
+const serviceAccount = {
+    type: 'service_account',
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
+console.log({serviceAccount})
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
